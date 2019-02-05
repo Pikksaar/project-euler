5f7d2980fdd02918bb602a4d9d04bb58b1e4c9f2
Mateo Gonzales <mgonzalesn17@outlook.com>
2019-02-05 16:44:27 -0500

    Changed included libraries

--- problems/003.cpp
+++ problems/003.cpp
@@ -1,5 +1,4 @@
-a #include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
--- problems/013.cpp
+++ problems/013.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 #define SIZE 100
 #define DIGITS_PER_NUMBER 50
